62130dea7000f3b7c22736997955da063db14c05
JosseG <jq994435258@gmail.com>
2021-09-26 22:39:16 -0500

    Query Actualizado

--- BD/ReservaHoteles.sql
+++ BD/ReservaHoteles.sql
@@ -3,9 +3,10 @@
 create database ReservaHotel;
 
 create table direccion(
-id_direccion char(4) primary key,
+id_direccion char(8) primary key,
 descripcion_direccion varchar(50) not null,
-distrito_direccion varchar(30) not null
+provincia_direccion varchar(50) not null,
+pais_direccion varchar(30) not null
 );
 
 create table cliente(
@@ -15,7 +16,7 @@ apellidoPat_cliente varchar(60) not null,
 apellidoMat_cliente varchar(60) not null,
 telefono_cliente varchar(9) not null,
 correo_cliente varchar(90),
-id_direccion char(4) not null,
+id_direccion char(8) not null,
 foreign key(id_direccion) references direccion(id_direccion)
 );
 
@@ -36,7 +37,7 @@ nombre_rol varchar(15) not null
 create table empleado(
 id_emp char(3) primary key not null,
 id_rol char(2) not null,
-dni_emp char(6) not null,
+di_emp char(11) not null,
 nombre_emp varchar(40) not null,
 apellidoPat_emp varchar(60) not null,
 apellidoMat_emp varchar(60) not null,
@@ -46,12 +47,19 @@ fecha_ingreso_emp date not null,
 foreign key(id_rol) references rol(id_rol)
 );
 
-create table login(
-id_login int primary key auto_increment,
+create table cuenta(
+id_cuenta char(4) primary key not null,
 id_emp char(3) not null,
 username_login varchar(30) not null,
 password_login varchar(30) not null,
 foreign key(id_emp) references empleado(id_emp)
+);
+
+create table login(
+id_login int primary key auto_increment,
+id_cuenta char(4) not null,
+fecha_login datetime not null,
+foreign key(id_cuenta) references cuenta(id_cuenta)
 );
 
 
@@ -71,10 +79,12 @@ id_cliente int not null,
 id_hab char(4) not null,
 precioHab_dBoleta double not null,
 precioHabExtra_dBoleta double not null,
+foreign key(id_boleta) references boleta(id_boleta),
 foreign key(id_cliente) references cliente(id_cliente),
 foreign key(id_hab) references habitacion(id_hab),
 primary key(id_boleta,id_cliente,id_hab)
 );
+
 
 use ReservaHotel;
 
